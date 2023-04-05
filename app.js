463c3286e9958ae9879bbddc4b5e38d08be84e1e
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-04-05 17:00:37 +0300

    feat: add animation & styles to additional items

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Объявляем переменные
 let myInput = document.querySelector('input');
-let doneItem = document.querySelector('.done');
-let failedItem = document.querySelector('.failed');
-let newItem = document.querySelector('.new-item');
+// let submitItem = document.querySelectorAll('.submit');
+// let cancelItem = document.querySelectorAll('.cancel');
+// let newItem = document.querySelectorAll('.new-item');
+// let check = document.querySelectorAll('.check');
 
 // Срабатывание функции при клике по кнопке
 document.querySelector('#do-btn').addEventListener('click', () => {
@@ -12,7 +13,7 @@ document.querySelector('#do-btn').addEventListener('click', () => {
 // Срабатывание функции при нажатии на Enter
 myInput.addEventListener('keydown', (event) => {
     if ( event.key == 'Enter') {
-        addItem();
+        addItem(); //Выполенится функция addItem()
     }
 })
 
@@ -21,55 +22,61 @@ myInput.addEventListener('keydown', (event) => {
 function addItem() {
 
     // создаём новый элемент div и присваиваем ему класс
-   let newDiv = document.createElement('div');
-   newDiv.className = "new-item";
+   let item = document.createElement('div');
+   item.className = "new-item";
 
     // создаём новый элемент p и и меняем ему текст
-   let newP = document.createElement('p');
-   newP.textContent = `to ${myInput.value}`;
+   let text = document.createElement('p');
+   text.textContent = `to ${myInput.value}`;
 
     // создаём новый элемент div и присваиваем ему класс
-   let newAdditionalDiv = document.createElement('div');
-   newAdditionalDiv.className = "new-item_additional";
+   let iconsContainer = document.createElement('div');
+   iconsContainer.className = "new-item_additional";
    
     // создаём новые элементы i и присваиваем им классы
-   let newI1 =document.createElement('i');
-   newI1.className = "fas fa-check done show";
-   let newI2 =document.createElement('i');
-   newI2.className = "fas fa-times failed hide";
-   let newI3 =document.createElement('i');
-   newI3.className = "fas fa-trash trash";
+    // submitIcon
+   let submitIcon =document.createElement('i');
+   submitIcon.className = "fas fa-check submit show";
 
-    // добавляем два элемента newP, newAdditionalDiv в родителя newDiv с помощью append(можно много элементом указывать)
-   newDiv.append(newP, newAdditionalDiv)
+    // При клике по submitIcon произойдёт изменения стилей
+   submitIcon.addEventListener('click', () => {
+    submitIcon.classList.toggle('show');
+    submitIcon.classList.toggle('hide');
+    cancelIcon.classList.toggle('hide');
+    cancelIcon.classList.toggle('show');
+    item.classList.toggle('mission-completed');
+   })
 
-   // добавляем три элемента newI1, newI2, newI3 в родителя newAdditionalDiv с помощью append(можно много элементом указывать)
-   newAdditionalDiv.append(newI1, newI2, newI3)
+// cancelIcon
+   let cancelIcon =document.createElement('i');
+   cancelIcon.className = "fas fa-times cancel hide";
+    // При клике по cancelIcon произойдёт изменения стилей
+   cancelIcon.addEventListener('click', () => {
+    submitIcon.classList.toggle('show');
+    submitIcon.classList.toggle('hide');
+    cancelIcon.classList.toggle('hide');
+    cancelIcon.classList.toggle('show');
+    item.classList.toggle('mission-completed');
+   })
 
-    //добавляем newDiv в конце родителя document.querySelector('.list') с помощью appendChild (добавить только один элемент можно)
-   document.querySelector('.list').appendChild(newDiv);
+    // trashIcon
+   let trashIcon =document.createElement('i');
+   trashIcon.className = "fas fa-trash trash";
+   // При клике по trashIcon произойдёт изменения стилей
+   trashIcon.addEventListener('click', () => {
+    item.remove();
+   })
+   
+
+    // добавляем два элемента text, iconsContainer в родителя item с помощью append(можно много элементом указывать)
+    item.append(text, iconsContainer)
+
+   // добавляем три элемента submitIcon, cancelIcon, trashIcon в родителя iconsContainer с помощью append(можно много элементом указывать)
+   iconsContainer.append(submitIcon, cancelIcon, trashIcon)
+
+    //добавляем item в конце родителя document.querySelector('.list') с помощью appendChild (добавить только один элемент можно)
+   document.querySelector('.list').appendChild(item);
 
     // После выполнения функции очищаем input
    myInput.value = '';
-   
 }
-
-// надо сделать переборку события по всем элементам через for
-
-// Обработка клика doneItem и добавление анимации 
-doneItem.addEventListener('click', () => {
-    doneItem.classList.toggle('show');
-    doneItem.classList.toggle('hide');
-    failedItem.classList.toggle('hide');
-    failedItem.classList.toggle('show');
-    newItem.classList.toggle('mission-completed');
-})
-
-// Обработка клика failedItem и добавление анимации 
-failedItem.addEventListener('click', () => {
-    doneItem.classList.toggle('show');
-    doneItem.classList.toggle('hide');
-    failedItem.classList.toggle('hide');
-    failedItem.classList.toggle('show');
-    newItem.classList.toggle('mission-completed');
-})
